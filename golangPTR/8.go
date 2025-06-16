093ab4e479b8f3f4763820a7322fbcc39a9a64cb
alinosi <146401086+alinosi@users.noreply.github.com>
2025-06-16 19:43:37 +0700

    Add initial implementation of pointer usage in Go

---
+++ golangPTR/8.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 42
+    var p *int = &a
+    fmt.Println("Nilai p:", *p)
+    
+    if p != nil {
+        fmt.Println("Nilai yang ditunjuk:", *p)
+    } else {
+        fmt.Println("Pointer nil!")
+    }
+    
+    // Buat p menunjuk ke nilai 42
+    // Kemudian print nilai yang ditunjuk
+}
+
+
+// saya kurang yakin dengan penyelesaian nomor 8
